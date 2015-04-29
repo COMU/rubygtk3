5e7172019010c9f2fbc8328e5720a87387a41c3d
Ebru Akagunduz <ebru.akagunduz@gmail.com>
2015-04-29 16:09:32 +0300

    add box example
    Signed-off-by: Ebru Akagunduz <ebru.akagunduz@gmail.com>

---
+++ source/boxes.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/ruby
+require "gtk3"
+
+win = Gtk::Window.new
+hbox = Gtk::Box.new(:horizontal, 3)
+hello_button = Gtk::Button.new(:label => "Hello World")
+goobye_button = Gtk::Button.new(:label => "Goodbye")
+
+win.add(hbox)
+hbox.pack_start(hello_button, :expand => true, :fill => false, :padding =>2)
+hbox.pack_start(goobye_button, :expand => true, :fill => false, :padding =>2)
+
+# print hello world to terminal
+hello_button.signal_connect("clicked") {puts "Hello World"}
+goobye_button.signal_connect("clicked") {puts "Goodbye"}
+
+win.signal_connect("destroy"){Gtk.main_quit}
+win.show_all
+Gtk.main 
